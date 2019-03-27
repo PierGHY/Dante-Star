487e8e21930a07908e0eb7b48d98014827eb51cb
GHYZEl Pierre <gpierre@localhost.localdomain>
2019-03-27 15:14:30 +0100

    zzzz

--- main.c
+++ main.c
@@ -7,13 +7,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
-/*
-char ** str() 
+char ** to_txt(char ** str)
 {
+    int i = 0;
 
+    int fd = open("maze.txt", O_WRONLY);
+    if (fd == -1)
+        return (84);
+    while (str[i] != NULL) {
+        write(fd, str[i], strlen(str[i]));
+        i++;
+    }
 }
-*/
 
 void load_2d_arr_from_file(char ** av)
 {
@@ -23,9 +32,10 @@ void load_2d_arr_from_file(char ** av)
     int i = 0;
 
     while (i != atoi(av[2])) {
-        str[i] = malloc(sizeof(char) * (atoi(av[1])));
+        str[i] = malloc(sizeof(char) * (atoi(av[1]) + 1));
         i = i + 1;
     }
+    str[i] = NULL;
     disp(str, av);
 }
 
@@ -36,8 +46,10 @@ void disp(char ** str, char ** av)
     int k = 0;
 
     while (i != atoi(av[2])) {
-        while (j != atoi(av[1])) {
-            if (j == 0 && i == 0 || j == 0 || i == atoi(av[2]) -1)
+        while (j != atoi(av[1]) + 1) {
+            if (j == atoi(av[1]))
+                str[i][j] = '\n';
+            else if (j == 0 && i == 0 || j == 0 || i == atoi(av[2]) -1)
                 str[i][j] = '*';
             else
                 str[i][j] = 'X';
@@ -48,10 +60,10 @@ void disp(char ** str, char ** av)
         i = i + 1;
     }
     for (int h = 0; h < atoi(av[2]); h++) {
-        for (int z = 0; z < atoi(av[1]); z++)
+        for (int z = 0; z < atoi(av[1]) + 1; z++)
             printf("%c", str[h][z]);
-        printf("\n");
     }
+    to_txt(str);
     return (str);
 }
 int main(int ac, char ** av)
