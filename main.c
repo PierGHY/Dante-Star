7f1cb8b64cf9b8939ca63e9658783bd662addb31
GHYZEl Pierre <gpierre@localhost.localdomain>
2019-03-28 17:13:04 +0100

    norm

--- main.c
+++ main.c
@@ -42,34 +42,28 @@ void load_2d_arr_from_file(char ** av)
 
 void disp(char ** str, char ** av)
 {
-    int i = 0;
-    int j = 0;
-    int k = 0;
     int r = 0;
 
     srand(time(NULL));
-    while (i != atoi(av[2])) {
-        while (j != atoi(av[1]) + 1) {
+    for (int i = 0; i != atoi(av[2]); i++) {
+        for (int j = 0; j != atoi(av[1]) + 1; j++) {
             r = rand();
             if (j == atoi(av[1]))
                 str[i][j] = '\n';
-            else if (j == 0 && i == 0 || j == 0 || i == atoi(av[2]) -1 || r % 2 == 0)
+            else if (j == 0 && i == 0 || j == 0 ||
+            i == atoi(av[2]) -1 || r % 2 == 0)
                 str[i][j] = '*';
             else
                 str[i][j] = 'X';
-            j = j + 1;
-            k = k + 1;
         }
-        j = 0;
-        i = i + 1;
     }
-    for (int h = 0; h < atoi(av[2]); h++) {
+    for (int h = 0; h < atoi(av[2]); h++)
         for (int z = 0; z < atoi(av[1]) + 1; z++)
             printf("%c", str[h][z]);
-    }
     to_txt(str);
     return (str);
 }
+
 int main(int ac, char ** av)
 {
     if (ac != 2)
