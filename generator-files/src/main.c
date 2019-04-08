0cab9275cc0d897a6621e7ef1f65ef8d5abf8c2a
GHYZEl Pierre <gpierre@localhost.localdomain>
2019-04-08 12:26:49 +0200

    perfect and imperfect mazes

--- generator-files/src/main.c
+++ generator-files/src/main.c
@@ -32,7 +32,32 @@ void disp(char **str, char **av)
         printf("%s", str[h]);
 }
 
-void load_2d_arr_from_file(char **av)
+void disp_np(char **str, char **av)
+{
+    int r = 0;
+    int z = 0;
+
+    srand(time(NULL));
+    for (int i = 0; i != atoi(av[2]); i++) {
+        for (int j = 0; j != atoi(av[1]) + 1; j++) {
+            r = rand();
+            if (j == atoi(av[1]))
+                str[i][j] = '\n';
+            else if ((j == 0 && i == 0) || i%3 != 0 ||
+                ((j == atoi(av[1]) - 1 && i == atoi(av[2]) -1)))
+                str[i][j] = '*';
+            else
+                str[i][j] = 'X';
+        }
+        z = r % atoi(av[1]);
+        if (i%3 == 0)
+            str[i][z] = '*';
+    }
+    for (int h = 0; h < atoi(av[2]); h++)
+        printf("%s", str[h]);
+}
+
+void load_2d_arr_from_file(char **av, int ac)
 {
     char **str = malloc(sizeof(char *) * (atoi(av[2])));
     int i = 0;
@@ -42,13 +67,16 @@ void load_2d_arr_from_file(char **av)
         i = i + 1;
     }
     str[i] = NULL;
-    disp(str, av);
+    if (ac == 4)
+        disp(str, av);
+    if (ac == 3)
+        disp_np(str, av);
 }
 
 int main(int ac, char **av)
 {
-    if (ac != 2)
-        load_2d_arr_from_file(av);
+    if (ac == 3 || ac == 4)
+        load_2d_arr_from_file(av, ac);
     else
         return (84);
     return (0);
